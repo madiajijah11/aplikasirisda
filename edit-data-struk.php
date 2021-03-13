3a13fabf4450d18e92d644d75b5c0e652d422a2c
Dian Rahmadani <madiajijah7@gmail.com>
2021-03-13 13:39:23 +0800

    Update edit-data-struk.php

--- edit-data-struk.php
+++ edit-data-struk.php
@@ -32,33 +32,33 @@ if (isset($_POST['editstruk'])) {
       </div>
       <div class="card-body">
         <form class="form-item" action="" method="post" role="form">
-          <input type="hidden" name="id" id="id" value="<?= $row['id']; ?>">
+          <input type="hidden" name="id" id="id" value="<?= $row['id'] ?>">
           <div class="form-group">
             <label for="nama">Nama Lengkap</label>
-            <input type="text" class="form-control" name="nama" required="required" value="<?= $row['nama']; ?>">
+            <input type="text" class="form-control" name="nama" required="required" value="<?= $row['nama'] ?>">
           </div>
           <div class="form-group">
             <label for="alamat">Alamat</label>
-            <textarea class="form-control" name="alamat" required="required"><?= $row['alamat']; ?></textarea>
+            <textarea class="form-control" name="alamat" required="required"><?= $row['alamat'] ?></textarea>
           </div>
           <div class="form-group">
             <label for="norekening">Nomor Rekening</label>
-            <input type="number" class="form-control" name="norekening" required="required" value="<?= $row['norekening']; ?>">
+            <input type="number" class="form-control" name="norekening" required="required" value="<?= $row['norekening'] ?>">
           </div>
           <div class="form-group row-cols-lg-5">
             <label for="pinjaman">Pinjaman</label>
             <select name="pinjaman" id="" required="required" class="form-control">
               <option selected disabled>Pilih</option>
-              <option value="5000000 <?php if(isset($row['pinjaman']) && ($row['pinjaman']) == '5000000') echo 'selected="selected"'; ?>">5.000.000</option>
-              <option value="10000000 <?php if(isset($row['pinjaman']) && ($row['pinjaman']) == '10000000') echo 'selected="selected"'; ?>">10.000.000</option>
-              <option value="15000000 <?php if(isset($row['pinjaman']) && ($row['pinjaman']) == '15000000') echo 'selected="selected"'; ?>">15.000.000</option>
-              <option value="20000000 <?php if(isset($row['pinjaman']) && ($row['pinjaman']) == '20000000') echo 'selected="selected"'; ?>">20.000.000</option>
-              <option value="25000000 <?php if(isset($row['pinjaman']) && ($row['pinjaman']) == '25000000') echo 'selected="selected"'; ?>">25.000.000</option>
-              <option value="30000000 <?php if(isset($row['pinjaman']) && ($row['pinjaman']) == '30000000') echo 'selected="selected"'; ?>">30.000.000</option>
-              <option value="35000000 <?php if(isset($row['pinjaman']) && ($row['pinjaman']) == '35000000') echo 'selected="selected"'; ?>">35.000.000</option>
-              <option value="40000000 <?php if(isset($row['pinjaman']) && ($row['pinjaman']) == '40000000') echo 'selected="selected"'; ?>">40.000.000</option>
-              <option value="45000000 <?php if(isset($row['pinjaman']) && ($row['pinjaman']) == '45000000') echo 'selected="selected"'; ?>">45.000.000</option>
-              <option value="50000000 <?php if(isset($row['pinjaman']) && ($row['pinjaman']) == '50000000') echo 'selected="selected"'; ?>">50.000.000</option>
+              <option value="5000000"<?php if(isset($row['pinjaman']) && ($row['pinjaman']) == '5000000') echo 'selected="selected"'; ?>>5.000.000</option>
+              <option value="10000000" <?php if(isset($row['pinjaman']) && ($row['pinjaman']) == '10000000') echo 'selected="selected"'; ?>>10.000.000</option>
+              <option value="15000000" <?php if(isset($row['pinjaman']) && ($row['pinjaman']) == '15000000') echo 'selected="selected"'; ?>>15.000.000</option>
+              <option value="20000000" <?php if(isset($row['pinjaman']) && ($row['pinjaman']) == '20000000') echo 'selected="selected"'; ?>>20.000.000</option>
+              <option value="25000000" <?php if(isset($row['pinjaman']) && ($row['pinjaman']) == '25000000') echo 'selected="selected"'; ?>>25.000.000</option>
+              <option value="30000000" <?php if(isset($row['pinjaman']) && ($row['pinjaman']) == '30000000') echo 'selected="selected"'; ?>>30.000.000</option>
+              <option value="35000000" <?php if(isset($row['pinjaman']) && ($row['pinjaman']) == '35000000') echo 'selected="selected"'; ?>>35.000.000</option>
+              <option value="40000000" <?php if(isset($row['pinjaman']) && ($row['pinjaman']) == '40000000') echo 'selected="selected"'; ?>>40.000.000</option>
+              <option value="45000000" <?php if(isset($row['pinjaman']) && ($row['pinjaman']) == '45000000') echo 'selected="selected"'; ?>>45.000.000</option>
+              <option value="50000000" <?php if(isset($row['pinjaman']) && ($row['pinjaman']) == '50000000') echo 'selected="selected"'; ?>>50.000.000</option>
             </select>
             <!-- <input type="text" class="form-control" name="pinjaman" placeholder="Kawin/Belum Kawin/Cerai" required="required"> -->
           </div>
@@ -66,17 +66,17 @@ if (isset($_POST['editstruk'])) {
             <label for="jangkawaktu">Jangka Waktu</label>
             <select name="jangkawaktu" id="" required="required" class="form-control">
               <option selected disabled>Pilih</option>
-              <option value="12 Bulan <?php if(isset($row['jangkawaktu']) && ($row['jangkawaktu']) == '12 Bulan') echo 'selected="selected"'; ?>">12 Bulan</option>
-              <option value="24 Bulan <?php if(isset($row['jangkawaktu']) && ($row['jangkawaktu']) == '24 Bulan') echo 'selected="selected"'; ?>">24 Bulan</option>
-              <option value="36 Bulan <?php if(isset($row['jangkawaktu']) && ($row['jangkawaktu']) == '36 Bulan') echo 'selected="selected"'; ?>">36 Bulan</option>
-              <option value="48 Bulan <?php if(isset($row['jangkawaktu']) && ($row['jangkawaktu']) == '48 Bulan') echo 'selected="selected"'; ?>">48 Bulan</option>
-              <option value="60 Bulan <?php if(isset($row['jangkawaktu']) && ($row['jangkawaktu']) == '60 Bulan') echo 'selected="selected"'; ?>">60 Bulan</option>
+              <option value="12 Bulan" <?php if(isset($row['jangkawaktu']) && ($row['jangkawaktu']) == '12 Bulan') echo 'selected="selected"'; ?>>12 Bulan</option>
+              <option value="24 Bulan" <?php if(isset($row['jangkawaktu']) && ($row['jangkawaktu']) == '24 Bulan') echo 'selected="selected"'; ?>>24 Bulan</option>
+              <option value="36 Bulan" <?php if(isset($row['jangkawaktu']) && ($row['jangkawaktu']) == '36 Bulan') echo 'selected="selected"'; ?>>36 Bulan</option>
+              <option value="48 Bulan" <?php if(isset($row['jangkawaktu']) && ($row['jangkawaktu']) == '48 Bulan') echo 'selected="selected"'; ?>>48 Bulan</option>
+              <option value="60 Bulan" <?php if(isset($row['jangkawaktu']) && ($row['jangkawaktu']) == '60 Bulan') echo 'selected="selected"'; ?>>60 Bulan</option>
             </select>
             <!-- <input type="text" class="form-control" name="jangkawaktu" placeholder="3 Bulan" required="required"> -->
           </div>
           <div class="form-group row-cols-lg-5">
             <label for="tgljatuhtempo">Tanggal Jatuh Tempo</label>
-            <input type="date" class="form-control" name="tgljatuhtempo" pattern="\d{1,2}-\d{1,2}-\d{4}" value="<?= $row['tgljatuhtempo']; ?>">
+            <input type="date" class="form-control" name="tgljatuhtempo" pattern="\d{1,2}-\d{1,2}-\d{4}" required="required" value="<?= $row['tgljatuhtempo'] ?>">
           </div>
           <button type="submit" name="editstruk" class="btn btn-primary" onclick="return confirm('Simpan perubahan?')">Edit</button>
           <a href="data-struk.php" class="btn btn-warning" onclick="return confirm('Yakin batal edit?')">Kembali</a>
